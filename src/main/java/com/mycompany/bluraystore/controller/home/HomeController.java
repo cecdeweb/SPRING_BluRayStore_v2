e6da3dc03d1389f93befe151910cdc0054e3bad7
Cécile Lafont <104426870+cecdeweb@users.noreply.github.com>
2022-06-27 09:16:32 +0200

    modularity application

--- src/main/java/com/mycompany/bluraystore/controller/home/HomeController.java
+++ src/main/java/com/mycompany/bluraystore/controller/home/HomeController.java
@@ -4,6 +4,7 @@ import com.mycompany.bluraystore.entity.Movie;
 import com.mycompany.bluraystore.service.MovieServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,10 +16,17 @@ public class HomeController {
     @Autowired
     private MovieServiceInterface service;
 
-    @RequestMapping("/bluraystore-home")
-    public @ModelAttribute("movies") List<Movie> displayHome(){
-        return service.getMovieList();
-    }
+// 1 ERE FACON DE FAIRE
+//    @RequestMapping("/bluraystore-home")
+//    public @ModelAttribute("movies") List<Movie> displayHome(){
+//        return service.getMovieList();
+//    }
 
+    // 2 EME FACON DE FAIRE
+    @RequestMapping("/bluraystore-home")
+    public String displayHome(Model model){
+        model.addAttribute("movies", service.getMovieList());
+        return "bluraystore-home";
+    }
 
 }
